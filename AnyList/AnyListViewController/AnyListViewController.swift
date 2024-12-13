912d6966e579e487fc6588b0388224220fe79b63
Bashur Viachaslau <bashvime@gmail.com>
2024-12-13 21:13:06 +0300

    подключил библиотеку Faerbase

--- AnyList/AnyListViewController/AnyListViewController.swift
+++ AnyList/AnyListViewController/AnyListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AnyListViewController.swift
 //  AnyList
 //
 //  Created by Вячеслав Башур on 13/12/2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AnyListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
