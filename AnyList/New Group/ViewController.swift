f17bdc05abaa25680c9baf8255b52748f6b308fc
Bashur Viachaslau <bashvime@gmail.com>
2024-12-13 18:36:31 +0300

    создал проект

---
+++ AnyList/New Group/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnyList
+//
+//  Created by Вячеслав Башур on 13/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
